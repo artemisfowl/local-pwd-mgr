fab3344ab5f47e691d169983861432f2bf68f587
artemisfowl <skb655952@gmail.com>
2020-02-17 20:06:34 +0000

    Broke up main into more functions

--- main.py
+++ main.py
@@ -39,97 +39,119 @@ def main():
 		"Do you wish this program to manage this password? [y/n/q/s] ")
 
 		if choice.lower() == 'y':
-			app_name = input("Application name : ")
-			print("Password to be managed for : {}".format(app_name))
-
-			# check for the application name in the JSON file - if the
-			# application is not present - say adding application to the list
-			if app_name in set(json_d.keys()):
-				print("found the application in the configuration file")
-				# if the application name is found, show the current password
-				# before asking for the changing of the password
-				print("Archiving current password for date : {}".format(
-					datetime.strftime(datetime.now(), "%Y-%m-%d")))
-
-				# get the value of the application name
-				print("Current object value of app_name : {}".format(
-					json_d.get(app_name)))
-				print("Last password that was set : {}".format(
-					json_d.get(app_name).get(CUR)))
-				print("Last set of excluded characters : {}".format(
-					json_d.get(app_name).get(EXCL)))
-				print("Last length of the password : {}".format(
-					json_d.get(app_name).get(LENP)))
-
-				# ask for the details now
-				napp_d = json_d.get(app_name)
-
-				len_passwd = int(input("Required length [>8] : "))
-				excl_chars = input("Characters to exclude : ")
-				gen_pwd = gen_passwd(len_passwd, excl_chars)
-
-				# Adding the older password in the date section
-				archive_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
-				napp_d[archive_date] = json_d.get(app_name).get(CUR)
-				napp_d[CUR] = gen_pwd
-				if excl_chars.lower() == 'null':
-					napp_d[EXCL] = ''
-				else:
-					napp_d[EXCL] += excl_chars
-				napp_d[LENP] = len_passwd
-
-				json_d[app_name] = napp_d
-
-				with open(RES_FILE, 'w') as f:
-					f.write(dumps(json_d, indent = 3))
-					f.write("\n")
-			else:
-				print("application name not found in the configuration file")
-				napp_d = dict()
-
-				len_passwd = int(input("Required length [>8] : "))
-				excl_chars = input("Characters to exclude : ")
-				gen_pwd = gen_passwd(len_passwd, excl_chars)
-				print("Generated password : {}".format(gen_pwd))
-
-				napp_d[CUR] = gen_pwd
-				napp_d[EXCL] = excl_chars
-				napp_d[LENP] = len_passwd
-
-				json_d[app_name] = napp_d
-
-				with open(RES_FILE, 'w') as f:
-					f.write(dumps(json_d, indent = 3))
-					f.write("\n")
+			manage_app(json_d)
 		elif choice.lower() == 'n':
-			print("Choice is No")
-
-			len_passwd = int(input("Required length [>8] : "))
-			excl_chars = input("Characters to exclude : ")
-			print("Generated password : {}".format(
-				gen_passwd(len_passwd, excl_chars)))
+			print("Generated password : {}".format(just_gen()))
 		elif choice.lower() == 'q':
 			break
 		elif choice.lower() == 's':
-			app_name = input('Application name : ')
-			print("Showing password for : {}".format(app_name))
-
-			print("Applications managed : {}".format(json_d.keys()))
-
-			if app_name in set(json_d.keys()):
-				print("Application is managed by this program")
-				print("Details of {} : {}".format(app_name,
-					json_d.get(app_name).get(CUR)))
-
-				# now copy this immediately in the clipboard
-
-				print("\nCurrent password has been copied in the clipboard\n")
-			else:
-				print("Applications being managed by this program : {}".format(
-					{i for i in json_d.keys()}))
+			show_app(json_d)
+		elif choice.lower() == 'a':
+			# create a show_all function to display the results of all the
+			# applications whose passwords are being managed by this program
+			pass
 
 	print("Exiting the application")
 	return 0
+
+def manage_app(json_d):
+	'''
+		@function manage_app
+		@brief function to manage the application passwords
+	'''
+	app_name = input("Application name : ")
+	print("Password to be managed for : {}".format(app_name))
+
+	# check for the application name in the JSON file - if the
+	# application is not present - say adding application to the list
+	if app_name in set(json_d.keys()):
+		print("found the application in the configuration file")
+		# if the application name is found, show the current password
+		# before asking for the changing of the password
+		print("Archiving current password for date : {}".format(
+			datetime.strftime(datetime.now(), "%Y-%m-%d")))
+
+		# get the value of the application name
+		print("Current object value of app_name : {}".format(
+			json_d.get(app_name)))
+		print("Last password that was set : {}".format(
+			json_d.get(app_name).get(CUR)))
+		print("Last set of excluded characters : {}".format(
+			json_d.get(app_name).get(EXCL)))
+		print("Last length of the password : {}".format(
+			json_d.get(app_name).get(LENP)))
+
+		# ask for the details now
+		napp_d = json_d.get(app_name)
+
+		len_passwd = int(input("Required length [>8] : "))
+		excl_chars = input("Characters to exclude (null to reset): ")
+		if excl_chars.lower() == 'null':
+			napp_d[EXCL] = ''
+		else:
+			napp_d[EXCL] += excl_chars
+		gen_pwd = gen_passwd(len_passwd, napp_d.get(EXCL))
+		print("Generated password : {}".format(gen_pwd))
+
+		# Adding the older password in the date section
+		archive_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+		napp_d[archive_date] = json_d.get(app_name).get(CUR)
+		napp_d[CUR] = gen_pwd
+		napp_d[LENP] = len_passwd
+
+		json_d[app_name] = napp_d
+
+		with open(RES_FILE, 'w') as f:
+			f.write(dumps(json_d, indent = 3))
+			f.write("\n")
+	else:
+		print("application name not found in the configuration file")
+		napp_d = dict()
+
+		len_passwd = int(input("Required length [>8] : "))
+		excl_chars = input("Characters to exclude : ")
+		gen_pwd = gen_passwd(len_passwd, excl_chars)
+		print("Generated password : {}".format(gen_pwd))
+
+		napp_d[CUR] = gen_pwd
+		napp_d[EXCL] = excl_chars
+		napp_d[LENP] = len_passwd
+
+		json_d[app_name] = napp_d
+
+		with open(RES_FILE, 'w') as f:
+			f.write(dumps(json_d, indent = 3))
+			f.write("\n")
+
+def just_gen():
+	'''
+		@function just_gen
+		@brief function to just generate the password
+	'''
+	len_passwd = int(input("Required length [>8] : "))
+	excl_chars = input("Characters to exclude : ")
+	return gen_passwd(len_passwd, excl_chars)
+
+def show_app(json_d):
+	'''
+		@function show_app
+		@brief function to show the current password setup for a specific
+		application
+	'''
+	app_name = input('Application name : ')
+	print("Showing password for : {}".format(app_name))
+
+	if app_name in set(json_d.keys()):
+		print("Application is managed by this program")
+		print("Details of {} : {}".format(app_name,
+			json_d.get(app_name).get(CUR)))
+
+		# now copy this immediately in the clipboard
+
+		#print("\nCurrent password has been copied in the clipboard\n")
+	else:
+		print("Applications being managed by this program : {}".format(
+			{i for i in json_d.keys()}))
 
 if __name__ == '__main__':
 	'''
